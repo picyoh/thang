4abd22a6341c227057ccee7a1e9f575f01713ecd
picyoh <picotyoh@gmail.com>
2026-03-04 23:21:49 +0100

    feat: :heavy_plus_sign: add mkcert
    add mkcert

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mkcert from 'vite-plugin-mkcert';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://picyoh.github.io',
+  base:'/thang',
+  vite: {
+    plugins: [tailwindcss(), mkcert()]
+  }
+});
