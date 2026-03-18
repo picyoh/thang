db57f6f84184c962526c2abc5062e79cffa3a9d8
picyoh <picotyoh@gmail.com>
2026-03-18 15:32:57 +0100

    feat: :sparkles: add time utility
    add time utility

---
+++ src/utils/time.ts
@@ -0,0 +1,37 @@
+function reformatTime(element: number) {
+  let currentEl = "";
+  element.toString().length === 1
+    ? (currentEl = `0${element}`)
+    : (currentEl = element.toString());
+  return currentEl;
+}
+
+const dateSpan = document.getElementById("date");
+// Get Date
+function getDate() {
+  const date = new Date();
+  const day = reformatTime(date.getDate());
+  const month = reformatTime(date.getMonth() + 1);
+  const year = reformatTime(date.getFullYear());
+  const currentDate = `${day}-${month}-${year}`;
+  dateSpan!.textContent = currentDate;
+}
+
+const timeSpan = document.getElementById("time");
+
+function getTime() {
+  const date = new Date();
+  const hours = reformatTime(date.getHours());
+  const minutes = reformatTime(date.getMinutes());
+  const seconds = reformatTime(date.getSeconds());
+  const currentTime = `${hours}:${minutes}:${seconds}`;
+  timeSpan!.textContent = currentTime;
+  setTimeout(() => {
+    getTime();
+    if (currentTime === "23:59:59") {
+      getDate();
+    }
+  }, 1000);
+}
+getDate();
+getTime();
