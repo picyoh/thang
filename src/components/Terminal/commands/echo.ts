4cb2ed8efa8b0d84dd68fa40a4359cb80d83b0b2
picyoh <picotyoh@gmail.com>
2026-03-25 10:16:32 +0100

    feat: :sparkles: add Time component
    add Time component

--- src/components/Terminal/commands/echo.ts
+++ src/components/Terminal/commands/echo.ts
@@ -1,5 +1,3 @@
-import * as help from "../../../datas/terminal/echo.md"
-
 export function echo(options: Array<string>, args: string) {
   // Remove all quotes
   let response = args.replaceAll('"', "").replaceAll("'", "");
@@ -11,24 +9,15 @@ export function echo(options: Array<string>, args: string) {
     response = response.replaceAll(" \\b", "");
     // Escape
     response = response.replaceAll("\\e", " ");
-    // Form feed
-    // ?
     // New line
     response = response.replaceAll("\\n", "<br>");
-    // Carriage return
-    // ?
     // Horizontal tab
     response = response.replaceAll("\\t", "&#09;");
-    // Vertical tab
-    // ?
-    //response = response.replaceAll("\\v", "&#11;");
     return response;
   } else if (options[0] === "\c") {
     // no output
     return "";
   } else if(options[0] === "--help"){
-    // Display help (echo.md)
-    console.log(help)
     return "--help";
   } else {
     // Wrong options
---
+++ src/components/Time/time.ts
@@ -0,0 +1,64 @@
+// TODO: add short formats option
+// TODO: add Temporal Object instead of Date
+
+function formatTime(
+  type: string,
+  format: string,
+  first: string,
+  second: string,
+  third: string,
+) {
+  let formated = "";
+  if(type === "time"){
+    // Time
+    formated = format.replace("hh",first).replace("mm", second).replace("ss", third);
+  }else if(type === "date"){
+    // Date
+    formated = format.replace("dd", first).replace("mm", second).replace("yy", third);
+  }
+  console.log(formated, first, second, third)
+  return formated;
+}
+
+// Get Date
+function getDate(format: string) {
+  const date = new Date();
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const year = String(date.getFullYear()).padStart(2, "0");
+  return formatTime("date",format, day, month, year);
+}
+
+// Update Date
+function updateDate(dateSpan: HTMLElement, format: string) {
+  const currentDate = getDate(format);
+  dateSpan!.textContent = currentDate;
+}
+
+// Get Time
+function getTime(format: string) {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  return formatTime("time",format, hours, minutes, seconds);
+}
+
+// Handle component
+export function handleTimeComponent(type: string, format: string) {
+  // Get HTML Elements
+  const dateSpan = document.getElementById("date-container");
+  const timeSpan = document.getElementById("time-container");
+  // Update Date
+  if (dateSpan?.textContent === "" && type === "date") {
+    updateDate(dateSpan, format);
+  }
+  // Update Time
+  if (type === "time") {
+    const currentTime = getTime(format);
+    timeSpan!.textContent = currentTime;
+    if (currentTime === "00:00:00") {
+      updateDate(dateSpan!, format);
+    }
+  }
+}
