a5252e4373e168a6bcc873f111a2d800447f14fb
picyoh <picotyoh@gmail.com>
2026-03-27 13:32:36 +0100

    feat: :sparkles: add mutation observer on taskbar
    add mutation observer on taskbar

---
+++ src/components/Taskbar/mutationObserver.ts
@@ -0,0 +1,35 @@
+// remove mocked datas
+const icons = [
+  { title: "Fastfetch", name: "id-card-rounded" },
+  { title: "Terminal", name: "terminal-rounded" },
+];
+
+function addIcons(node: HTMLElement) {
+  const iconBar = document.getElementById("iconBar");
+  const nodeId = node.id;
+  icons.forEach((icon) => {
+    if (icon.title === nodeId) {
+      const iconNode = iconBar?.querySelector(`#${nodeId}`);
+      const clonedNode = iconNode?.cloneNode(true) as HTMLElement;
+      iconBar?.insertAdjacentElement("beforeend", clonedNode!);
+    }
+  });
+}
+
+// Observer
+const observer = new MutationObserver((mutations) => {
+  for (const mutation of mutations) {
+    if (mutation.type === "childList" && mutation.addedNodes.length) {
+      mutation.addedNodes.forEach((node) => {
+        if (node.nodeType !== Node.ELEMENT_NODE) return;
+        const newNode = node as HTMLElement;
+        addIcons(newNode);
+      });
+    }
+  }
+});
+
+// Export function
+export function observeChanges(main: HTMLElement) {
+  observer.observe(main!, { childList: true, subtree: true });
+}
